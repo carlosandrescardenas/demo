0898b664a8f3d2718e950a3030822056d7bdc48a
cpacho1 <carlos.pachon@gmfinancial.com>
2022-08-05 14:22:55 -0500

    first commit

---
+++ src/test/java/com/testspringboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testspringboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
